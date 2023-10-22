cac45c9213f9585e979d6cc813796106aabf7c0a
dk8827 <121302433+dk8827@users.noreply.github.com>
2023-10-22 09:46:04 +0300

    refactor

--- carsgame.py
+++ carsgame.py
@@ -1,12 +1,11 @@
-
 import pygame
-import random
 import math
 import random
 import os
 import importlib
 import inspect
 from abc import ABC, abstractmethod
+
 # Initialize pygame
 pygame.init()
 
@@ -22,32 +21,32 @@ LAP_COUNT = 3
 INITIAL_MISSILE_COUNT = 8
 
 INITIAL_CAR_SPEED = 0
-CAR_REGULAR_MAX_SPEED=4
+CAR_REGULAR_MAX_SPEED = 4
 CHECKPOINT_RADIUS = 10
 DISTANCE_FROM_CHECKPOINT = 60
-NITRO_SPEED=3
-NITRO_DURATION=2000
-GAS_SPEED_INCREASE=0.005
-INCLUDE_ONLY_NON_CPU_CARS=False
-MISSILE_HIT_EFFECT_DURATION=1000
+NITRO_SPEED = 3
+NITRO_DURATION = 2000
+GAS_SPEED_INCREASE = 0.005
+INCLUDE_ONLY_NON_CPU_CARS = False
+MISSILE_HIT_EFFECT_DURATION = 1000
 
 OIL_SPILL_DURATION = 2000
 OIL_SPILL_RADIUS = 25
 OIL_SPILL_COLOR = (0, 0, 0)
 
-ROTATE_RIGHT="ROTATE_RIGHT"
-ROTATE_LEFT="ROTATE_LEFT"
-GAS="GAS"
-DROP_OIL="DROP_OIL"
-ACTIVATE_NITRO="ACTIVATE_NITRO"
-SHOOT_MISSILE="SHOOT_MISSILE"
-MISSILE_SHOT_COOLDOWN=500
-        
+ROTATE_RIGHT = "ROTATE_RIGHT"
+ROTATE_LEFT = "ROTATE_LEFT"
+GAS = "GAS"
+DROP_OIL = "DROP_OIL"
+ACTIVATE_NITRO = "ACTIVATE_NITRO"
+SHOOT_MISSILE = "SHOOT_MISSILE"
+MISSILE_SHOT_COOLDOWN = 500
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Racing Game")
 clock = pygame.time.Clock()
 leaderboardFont = pygame.font.SysFont(None, 35)
-carNameFont=pygame.font.SysFont(None, 25)
+carNameFont = pygame.font.SysFont(None, 25)
 
 INNER_BOUNDARY = [
     (150, 100),
@@ -76,20 +75,24 @@ CHECKPOINTS = [
 ]
 
 spills = []
-cars=[]
+cars = []
 inner_surface_for_collision_checks = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
-outer_surface_for_collision_checks = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA) 
+outer_surface_for_collision_checks = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
 pygame.draw.polygon(outer_surface_for_collision_checks, (0, 0, 0), OUTER_BOUNDARY)
 pygame.draw.polygon(inner_surface_for_collision_checks, (0, 0, 0), INNER_BOUNDARY)
+
+
 class GameState:
     def __init__(self, cars):
         self.cars = cars
-        
+
+
 class CarController(ABC):
     @abstractmethod
     def decide_what_to_do_next(self, gamestate: GameState) -> str:
         pass
-    
+
+
 def load_car_controllers_from_directory():
     student_car_controllers = []
     directory = "car-definitions"
@@ -105,9 +108,7 @@ def load_car_controllers_from_directory():
             car_controller_class = None
             for name, obj in inspect.getmembers(student_car_module):
                 if inspect.isclass(obj):
-                    if obj.__name__ == 'CarController':
-                        car_controller_class = obj
-                    elif hasattr(obj, 'decide_what_to_do_next'):
+                    if hasattr(obj, 'decide_what_to_do_next'):
                         student_car_class = obj
                         break
 
@@ -124,15 +125,17 @@ def load_car_controllers_from_directory():
 def dot_product(v1, v2):
     return v1[0] * v2[0] + v1[1] * v2[1]
 
+
 def track_direction_at_point(x, y):
-    closest_points = sorted(OUTER_BOUNDARY, key=lambda point: (point[0]-x)**2 + (point[1]-y)**2)[:2]
+    closest_points = sorted(OUTER_BOUNDARY, key=lambda point: (point[0] - x) ** 2 + (point[1] - y) ** 2)[:2]
     dir_vector = (closest_points[1][0] - closest_points[0][0], closest_points[1][1] - closest_points[0][1])
-    magnitude = math.sqrt(dir_vector[0]**2 + dir_vector[1]**2)
+    magnitude = math.sqrt(dir_vector[0] ** 2 + dir_vector[1] ** 2)
     return (dir_vector[0] / magnitude, dir_vector[1] / magnitude)
 
 
 MISSILE_SPEED = 10
 MISSILE_LIFETIME = 5000  # 5 seconds
+
 
 class Missile:
     def __init__(self, x, y, angle):
@@ -149,6 +152,9 @@ class Missile:
         return pygame.time.get_ticks() - self.creation_time > MISSILE_LIFETIME
 
     def draw(self):
+        # if the missile has left the screen, don't draw it
+        if self.x < 0 or self.x > SCREEN_WIDTH or self.y < 0 or self.y > SCREEN_HEIGHT:
+            return
         pygame.draw.circle(screen, (200, 0, 0), (int(self.x), int(self.y)), 5)
 
 
@@ -166,123 +172,118 @@ class Spill:
 
 
 class Car:
-    def __init__(self, name,controller):
-        self.image = pygame.image.load("car.png") 
+    def __init__(self, name, controller):
+        self.image = pygame.image.load("car.png")
         self.rect = self.image.get_rect()
         self.place_on_track()
         self.speed = INITIAL_CAR_SPEED
         self.angle = 0  # Start driving to the right
         self.laps = 0
-        
 
         self.name = name
         self.next_checkpoint = 0  # index of the next checkpoint to cross
         self.checkpoints_crossed = 0  # how many checkpoints the car has crossed so far
         self.has_oil_spill = True
         self.on_oil_spill = False  # Is the car currently on an oil spill?
-        self.nitrosLeft=4
-        self.usingNitro=False
-        self.nitroUsedTime=pygame.time.get_ticks()
-        self.regularSpeed=INITIAL_CAR_SPEED
-        self.controller=controller
+        self.nitrosLeft = 4
+        self.usingNitro = False
+        self.nitroUsedTime = pygame.time.get_ticks()
+        self.regularSpeed = INITIAL_CAR_SPEED
+        self.controller = controller
         # Cache car name text
         self.name_text = carNameFont.render(self.name, True, (0, 0, 0))
         self.missiles = []
         self.missile_count = INITIAL_MISSILE_COUNT
         self.hit_time = None
-        self.missileShotTime=pygame.time.get_ticks()
+        self.missileShotTime = pygame.time.get_ticks()
 
     def shoot_missile(self):
-        if self.missile_count > 0 and pygame.time.get_ticks()-self.missileShotTime>MISSILE_SHOT_COOLDOWN:
+        if self.missile_count > 0 and pygame.time.get_ticks() - self.missileShotTime > MISSILE_SHOT_COOLDOWN:
             missile = Missile(self.rect.centerx, self.rect.centery, self.angle)
             self.missiles.append(missile)
             self.missile_count -= 1
-            self.missileShotTime=pygame.time.get_ticks()
-            
+            self.missileShotTime = pygame.time.get_ticks()
+
     def place_on_track(self):
         while True:
-            randomX=random.randint(OUTER_BOUNDARY[0][0], INNER_BOUNDARY[0][0] )
-            randomY=random.randint(OUTER_BOUNDARY[0][1], INNER_BOUNDARY[0][1])
-            
-            if self.is_on_track(randomX,randomY):
+            randomX = random.randint(OUTER_BOUNDARY[0][0], INNER_BOUNDARY[0][0])
+            randomY = random.randint(OUTER_BOUNDARY[0][1], INNER_BOUNDARY[0][1])
+
+            if self.is_on_track(randomX, randomY):
                 self.rect.centerx = randomX
                 self.rect.centery = randomY
                 return
 
     def update(self):
-        chosenAction=None
-        chosenAction=self.controller.decide_what_to_do_next(GameState(cars))
-            
+        chosenAction = self.controller.decide_what_to_do_next(GameState(cars))
 
-        #Handle chosen action
-        if (chosenAction==SHOOT_MISSILE):
+        # Handle chosen action
+        if (chosenAction == SHOOT_MISSILE):
             self.shoot_missile()
-        if chosenAction==ACTIVATE_NITRO and self.nitrosLeft>0 and not self.usingNitro:
-            self.usingNitro=True
-            self.nitrosLeft-=1
-        elif chosenAction==DROP_OIL and self.has_oil_spill:
-            spills.append(Spill(self.rect.centerx - 40* math.cos(self.angle), self.rect.centery - 40* math.sin(self.angle)))
+        if chosenAction == ACTIVATE_NITRO and self.nitrosLeft > 0 and not self.usingNitro:
+            self.usingNitro = True
+            self.nitrosLeft -= 1
+        elif chosenAction == DROP_OIL and self.has_oil_spill:
+            spills.append(
+                Spill(self.rect.centerx - 40 * math.cos(self.angle), self.rect.centery - 40 * math.sin(self.angle)))
             self.has_oil_spill = False
-        elif (chosenAction==ROTATE_RIGHT):
+        elif (chosenAction == ROTATE_RIGHT):
             self.angle += math.pi / 16
-        elif (chosenAction==ROTATE_LEFT): 
-            self.angle -= math.pi / 16 
-        elif (chosenAction==GAS):
-            self.regularSpeed+=GAS_SPEED_INCREASE
-            if self.regularSpeed>CAR_REGULAR_MAX_SPEED:
-                self.regularSpeed=CAR_REGULAR_MAX_SPEED
-        
+        elif (chosenAction == ROTATE_LEFT):
+            self.angle -= math.pi / 16
+        elif (chosenAction == GAS):
+            self.regularSpeed += GAS_SPEED_INCREASE
+            if self.regularSpeed > CAR_REGULAR_MAX_SPEED:
+                self.regularSpeed = CAR_REGULAR_MAX_SPEED
+
         prev_x, prev_y = car.rect.centerx, car.rect.centery
-        
+
         self.move_forward()
-            
+
         self.handle_collisions()  # Check and handle collisions with other cars
 
         move_direction = (self.rect.centerx - prev_x, self.rect.centery - prev_y)
-        magnitude = math.sqrt(move_direction[0]**2 + move_direction[1]**2)
+        magnitude = math.sqrt(move_direction[0] ** 2 + move_direction[1] ** 2)
         if magnitude != 0:
             move_direction = (move_direction[0] / magnitude, move_direction[1] / magnitude)
             track_dir = track_direction_at_point(self.rect.centerx, self.rect.centery)
-        
-        
+
         checkpoint_x, checkpoint_y = CHECKPOINTS[self.next_checkpoint]
-        if math.sqrt((self.rect.centerx - checkpoint_x)**2 + (self.rect.centery - checkpoint_y)**2) < DISTANCE_FROM_CHECKPOINT:
+        if math.sqrt((self.rect.centerx - checkpoint_x) ** 2 + (
+                self.rect.centery - checkpoint_y) ** 2) < DISTANCE_FROM_CHECKPOINT:
             self.checkpoints_crossed += 1
             self.next_checkpoint = (self.next_checkpoint + 1) % len(CHECKPOINTS)
-            #print(f"Checkpoint {self.checkpoints_crossed} finished by car {self.name}")
+            # print(f"Checkpoint {self.checkpoints_crossed} finished by car {self.name}")
 
-        #if self.checkpoints_crossed == len(CHECKPOINTS):
-            #print(f"All checkpoints finished by car {self.name}")
+        # if self.checkpoints_crossed == len(CHECKPOINTS):
+        # print(f"All checkpoints finished by car {self.name}")
         # Check if the car has crossed the start line
-        if prev_x < INNER_BOUNDARY[0][0] and self.rect.centerx >= INNER_BOUNDARY[0][0] and self.checkpoints_crossed == len(CHECKPOINTS):
-            if not self.has_oil_spill: #Give an oil spill every lap
+        if prev_x < INNER_BOUNDARY[0][0] and self.rect.centerx >= INNER_BOUNDARY[0][
+            0] and self.checkpoints_crossed == len(CHECKPOINTS):
+            if not self.has_oil_spill:  # Give an oil spill every lap
                 self.has_oil_spill = True
             self.laps += 1
             self.checkpoints_crossed = 0
-        
 
-                        
         # Check if the car is on an oil spill
         self.on_oil_spill = False
         for spill in spills:
-            distance_to_spill = math.sqrt((self.rect.centerx - spill.x)**2 + (self.rect.centery - spill.y)**2)
+            distance_to_spill = math.sqrt((self.rect.centerx - spill.x) ** 2 + (self.rect.centery - spill.y) ** 2)
             if distance_to_spill <= OIL_SPILL_RADIUS:
                 self.on_oil_spill = True
                 break
-            
-        #After 2 seconds nitro should stop
-        if self.usingNitro and pygame.time.get_ticks()-self.nitroUsedTime>NITRO_DURATION:
-            self.usingNitro=False
 
-        
+        # After 2 seconds nitro should stop
+        if self.usingNitro and pygame.time.get_ticks() - self.nitroUsedTime > NITRO_DURATION:
+            self.usingNitro = False
+
         if self.usingNitro and not self.on_oil_spill:
-            self.speed = NITRO_SPEED*self.regularSpeed
-            print("Using nitro speed")
+            self.speed = NITRO_SPEED * self.regularSpeed
         elif self.on_oil_spill:
             self.speed = 0.2 * self.regularSpeed
         else:
             self.speed = self.regularSpeed
-        
+
         # Remove expired missiles
         self.missiles = [m for m in self.missiles if not m.is_expired()]
 
@@ -292,23 +293,20 @@ class Car:
         if self.hit_time:
             self.regularSpeed = 0  # If hit by a missile, set speed to 0 for 3 seconds
 
-
-
-
     def move_forward(self):
-        futureX=self.rect.centerx + self.speed * math.cos(self.angle)
-        futureY=self.rect.centery + self.speed * math.sin(self.angle)
+        futureX = self.rect.centerx + self.speed * math.cos(self.angle)
+        futureY = self.rect.centery + self.speed * math.sin(self.angle)
 
         if self.is_on_track(futureX, futureY):
             self.rect.centerx = futureX
-            self.rect.centery =futureY
+            self.rect.centery = futureY
 
     def handle_collisions(self):
         for other_car in cars:
             if other_car != self and self.rect.colliderect(other_car.rect):
                 overlap_x = other_car.rect.centerx - self.rect.centerx
                 overlap_y = other_car.rect.centery - self.rect.centery
-                overlap_distance = math.sqrt(overlap_x**2 + overlap_y**2)
+                overlap_distance = math.sqrt(overlap_x ** 2 + overlap_y ** 2)
 
                 # Calculate the unit vector of overlap direction
                 if overlap_distance != 0:
@@ -318,46 +316,44 @@ class Car:
 
                 # Move both cars away from each other based on the overlap
                 move_distance = overlap_distance / 2
-                
+
                 # Move the car only if the new position is on track
                 new_x = self.rect.centerx - overlap_direction[0] * move_distance
                 new_y = self.rect.centery - overlap_direction[1] * move_distance
                 if self.is_on_track(new_x, new_y):
                     self.rect.centerx = new_x
                     self.rect.centery = new_y
-                
+
                 other_car_new_x = other_car.rect.centerx + overlap_direction[0] * move_distance
                 other_car_new_y = other_car.rect.centery + overlap_direction[1] * move_distance
                 if self.is_on_track(other_car_new_x, other_car_new_y):
                     other_car.rect.centerx = other_car_new_x
                     other_car.rect.centery = other_car_new_y
-                    
-                #Get a random number from 8 to 11 inclusive
-                randomAngleDivision=random.randint(8,11)
+
+                # Get a random number from 8 to 11 inclusive
+                randomAngleDivision = random.randint(8, 11)
                 # Adjust angles to simulate the push effect (you can fine-tune this)
                 self.angle += math.pi / randomAngleDivision
                 other_car.angle -= math.pi / randomAngleDivision
 
     def is_on_track(self, x, y):
         return self.is_inside_outer_boundary(x, y) and not self.is_inside_inner_boundary(x, y)
-    
-    def is_inside_outer_boundary(self, x, y):
-        #If the pixel is out of screen boundaries, return false
-        if x<0 or x>SCREEN_WIDTH or y<0 or y>SCREEN_HEIGHT:
-            return False
-        
-        
 
-        return outer_surface_for_collision_checks.get_at((int(x), int(y))) == (0, 0, 0, 255)  # Check if the pixel at (x, y) is black
+    def is_inside_outer_boundary(self, x, y):
+        # If the pixel is out of screen boundaries, return false
+        if x < 0 or x > SCREEN_WIDTH or y < 0 or y > SCREEN_HEIGHT:
+            return False
+
+        return outer_surface_for_collision_checks.get_at((int(x), int(y))) == (
+        0, 0, 0, 255)  # Check if the pixel at (x, y) is black
 
     def is_inside_inner_boundary(self, x, y):
         # If the pixel is out of screen boundaries, return false
-        if x<0 or x>SCREEN_WIDTH or y<0 or y>SCREEN_HEIGHT:
+        if x < 0 or x > SCREEN_WIDTH or y < 0 or y > SCREEN_HEIGHT:
             return False
 
-        
-        return inner_surface_for_collision_checks.get_at((int(x), int(y))) == (0, 0, 0, 255)  # Check if the pixel at (x, y) is black
-
+        return inner_surface_for_collision_checks.get_at((int(x), int(y))) == (
+        0, 0, 0, 255)  # Check if the pixel at (x, y) is black
 
     def draw(self):
         rotated_image = pygame.transform.rotate(self.image, -math.degrees(self.angle))
@@ -367,22 +363,20 @@ class Car:
 
 
 def display_leaderboard():
-    
-    sorted_cars = sorted(cars, key=lambda car: car.laps*100+car.checkpoints_crossed, reverse=True)[:5]
+    sorted_cars = sorted(cars, key=lambda car: car.laps * 100 + car.checkpoints_crossed, reverse=True)[:5]
     y_start = 10
 
     for i, car in enumerate(sorted_cars):
-        
-        checkpointPercentage=round(car.checkpoints_crossed/len(CHECKPOINTS)*100)
-        text = leaderboardFont.render(f"{i+1}. {car.name} - {car.laps} laps %{checkpointPercentage}", True, (0, 0, 0))
+        checkpointPercentage = round(car.checkpoints_crossed / len(CHECKPOINTS) * 100)
+        text = leaderboardFont.render(f"{i + 1}. {car.name} - {car.laps} laps %{checkpointPercentage}", True, (0, 0, 0))
         y_start += 40
-        screen.blit(text, (10,y_start))
+        screen.blit(text, (SCREEN_WIDTH - 300, y_start))
 
 
-car_controllers=load_car_controllers_from_directory()
+car_controllers = load_car_controllers_from_directory()
 for car_controller in car_controllers:
-    carControllerId=car_controller.id
-    car=Car(carControllerId,car_controller)
+    carControllerId = car_controller.id
+    car = Car(carControllerId, car_controller)
     cars.append(car)
 
 running = True
@@ -398,7 +392,7 @@ while running:
     pygame.draw.polygon(screen, TRACK_COLOR, OUTER_BOUNDARY)
     pygame.draw.polygon(screen, BG_COLOR, INNER_BOUNDARY)
     pygame.draw.line(screen, START_LINE_COLOR, OUTER_BOUNDARY[0], INNER_BOUNDARY[0], 5)
-        # Draw checkpoints
+    # Draw checkpoints
     for checkpoint in CHECKPOINTS:
         pygame.draw.circle(screen, (0, 0, 255), checkpoint, CHECKPOINT_RADIUS)
 
@@ -408,29 +402,33 @@ while running:
 
     # Remove expired oil spills
     spills = [spill for spill in spills if not spill.is_expired()]
-    
+
     for car in cars:
         car.draw()
 
     # Missile updates
     for car in cars:
         for missile in car.missiles:
+            # If missile has left the scree,remove it
+            if missile.x < 0 or missile.x > SCREEN_WIDTH or missile.y < 0 or missile.y > SCREEN_HEIGHT:
+                car.missiles.remove(missile)
+                break
+
             missile.move()
 
             for other_car in cars:
-                if other_car != car and math.sqrt((other_car.rect.centerx - missile.x)**2 + (other_car.rect.centery - missile.y)**2) < 20:  # 20 is approx. the size of a car
+                if other_car != car and math.sqrt((other_car.rect.centerx - missile.x) ** 2 + (
+                        other_car.rect.centery - missile.y) ** 2) < 20:  # 20 is approx. the size of a car
                     other_car.hit_time = pygame.time.get_ticks()
                     car.missiles.remove(missile)
                     break
-    
+
     for car in cars:
         for missile in car.missiles:
             missile.draw()
-    
+
     display_leaderboard()
 
-
-    
     fps_text = pygame.font.SysFont(None, 25).render(f"FPS: {int(clock.get_fps())}", True, (0, 0, 0))
     screen.blit(fps_text, (SCREEN_WIDTH - 70, 10))
 
